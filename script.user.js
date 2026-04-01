e69172ac2840f9ddb32913e2bc25e26d450d81ba
Bern1213 <37059160+Bern1213@users.noreply.github.com>
2026-04-01 15:05:30 +0900

    크아악 버전오류...!!!!

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name         아카콘 검색기
 // @description  아카라이브 아카콘 이름 검색 기능
 // @namespace    http://tampermonkey.net/
-// @version      1.0.10
+// @version      1.1.0
 // @match        https://arca.live/b/*
 // @grant        GM_addStyle
 // @author       Bernadetta
